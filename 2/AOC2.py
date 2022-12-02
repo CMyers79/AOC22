3714a26285e90b8cca2c97dfe7361bf7d379f54e
CMyers79 <codermyers@gmail.com>
2022-12-01 21:18:30 -0800

    day 2

--- 2/AOC2.py
+++ 2/AOC2.py
@@ -1,2 +1,33 @@
 with open('input.txt') as file:
-    strings = file.readlines()
+    s = file.readlines()
+
+score = 0
+
+m = [l.split() for l in s]
+for n in m:
+    if n[1] == 'X':
+        if n[0] == 'A':
+            score += 3
+        elif n[0] == 'B':
+            score += 1
+        else:
+            score += 2
+
+    elif n[1] == 'Y':
+        if n[0] == 'A':
+            score += 4
+        elif n[0] == 'B':
+            score += 5
+        else:
+            score += 6
+    else:
+        if n[0] == 'A':
+            score += 8
+        elif n[0] == 'B':
+            score += 9
+        else:
+            score +=7
+
+
+print(score)
+
